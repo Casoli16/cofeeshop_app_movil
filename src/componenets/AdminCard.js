d083b90e977fecd6bc13829e44e61d379d5262ae
Susan Castillo <susancasoli@gmail.com>
2024-06-11 00:14:16 -0600

    Scrud completado, solo falta que se recargue la lista luego del create y update.

--- src/componenets/AdminCard.js
+++ src/componenets/AdminCard.js
@@ -1,30 +1,139 @@
-import React, {useEffect} from "react";
-import {Text, View, StyleSheet} from "react-native";
+import React, {useEffect, useState} from "react";
+import {Text, View, StyleSheet, TouchableOpacity, Alert, Modal} from "react-native";
 import Constants from "expo-constants";
 import * as API from '../../utilis/constantes';
+import {TextInput} from "react-native-paper";
 
-const AdminCard = ({item}) => {
+//Funcion que recibe como parametro el item y el loadedCards
+const AdminCard = ({item, loadedCards, updateList}) => {
+    //Declaracion de variables a utilizar y le pasamos la informacion traida de la api para que aparezca en los inputs.
+    const [idAdmin, setIdAdmin] = useState(0);
+    const [nombreAdmin, setNombreAdmin] = useState(item.nombre_administrador);
+    const [apellidoAdmin, setApellidoAdmin] = useState(item.apellido_administrador);
+    const [correoAdmin, setCorreoAdmin] = useState(item.correo_administrador);
+    const [aliasAdmin, setAliasAdmin] = useState(item.alias_administrador);
 
-    const api = API.IP;
+    //Declaracion de variable para el modal
+    const [modalVisible, setModalVisible] = useState(false);
 
-    useEffect(() => {
+    const ip = API.IP;
 
-    }, []);
+    //Funcion para eliminar un administrador
+    const deleteAdmin = async (idAdmin) => {
+        const formData = new FormData();
+        //Mandamos el form con el id admin que se ha pasado.
+        formData.append('idAdministrador', idAdmin);
+        // Mandamos la peticion a la api
+        const response = await fetch(`http://${ip}/coffeeshop/api/services/admin/administrador.php?action=deleteRow`,{
+            method: 'POST',
+            body: formData
+        });
+        const data = await response.json();
+
+        if(data.status) {
+            Alert.alert('Administrados eliminado correctamente');
+            //Actualizamos nuestra lista
+            updateList(prevData=>prevData.filter(item => item.id_administrador !== idAdmin));
+        } else {
+            Alert.alert('Ocurrió un error al eliminar al administrador', data.error);
+        }
+    }
+
+    //Funcion para actualizar un administrador
+    const updateAdmin = async (idAdmin) => {
+        const formData = new FormData();
+        formData.append('idAdministrador', idAdmin);
+        formData.append('nombreAdministrador', nombreAdmin);
+        formData.append('apellidoAdministrador', apellidoAdmin);
+        formData.append('correoAdministrador', correoAdmin);
+
+        const response = await fetch(`http://${ip}/coffeeshop/api/services/admin/administrador.php?action=updateRow`, {
+            method: 'POST',
+            body: formData
+        });
+
+        const data = await response.json();
+        if (data.status){
+            Alert.alert('Éxito', data.message);
+            setModalVisible(false);
+            updateList(prevData=>prevData.filter(item => item.id_administrador));
+        } else{
+            Alert.alert('Error', data.error);
+        }
+    }
+
     return (
+        //Accedemos la informacion que viene de la api.
         <View style={styles.itemContainer}>
             <Text>ID: {item.id_administrador}</Text>
             <Text>Nombre: {item.nombre_administrador}</Text>
             <Text>Apellido : {item.apellido_administrador}</Text>
             <Text>Correo: {item.correo_administrador}</Text>
+
+            <TouchableOpacity style={styles.deleteButton} onPress={()=> deleteAdmin(item.id_administrador)}>
+                <Text style={styles.buttonText}>Eliminar</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.updateButton} onPress={()=> setModalVisible(true)}>
+                <Text style={styles.buttonText}>Actualizar</Text>
+            </TouchableOpacity>
+
+            {/*MODAL*/}
+            <Modal
+                animationType='slide'
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => {setModalVisible(!modalVisible)}}>
+
+                <View style={styles.modalCenter}>
+                    <View style={styles.modalContainer}>
+                        <Text style={styles.title}>Agregar Administrador</Text>
+                        <TextInput
+                            label='Nombre'
+                            value= {nombreAdmin}
+                            onChangeText={setNombreAdmin}
+                            style={styles.input}
+                        />
+                        <TextInput
+                            label='Apellido'
+                            value= {apellidoAdmin}
+                            onChangeText={setApellidoAdmin}
+                            style={styles.input}
+                        />
+                        <TextInput
+                            label='Correo'
+                            value= {correoAdmin}
+                            onChangeText={setCorreoAdmin}
+                            style={styles.input}
+                            keyboardType={"email-address"}
+                        />
+                        <TextInput
+                            editable={false}
+                            label='Alias'
+                            value= {aliasAdmin}
+                            style={styles.input}
+                        />
+                        <View>
+                            <TouchableOpacity style={styles.addButton} onPress={()=>{updateAdmin(item.id_administrador)}}>
+                                <Text style={styles.textWhite}>Actualizar</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={styles.addButton} onPress={()=>{setModalVisible(false)}}>
+                                <Text style={styles.textWhite}>Cancelar</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </View>
+            </Modal>
+
         </View>
     );
 }
 export default AdminCard;
 
+//Estilos
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#EAD8C0',
+        backgroundColor: '#ffffff',
         paddingTop: Constants.statusBarHeight,
         paddingHorizontal: 16
     },
@@ -38,12 +147,56 @@ const styles = StyleSheet.create({
     itemContainer: {
         padding: 16,
         marginVertical: 8,
-        backgroundColor: '#fff',
+        backgroundColor: '#f8eded',
         borderRadius: 8,
         shadowColor: '#000',
         shadowOffset: {width: 0, height: 0},
         shadowOpacity: 0.1,
         shadowRadius: 4,
         elevation: 2,
-    }
+    },
+    deleteButton: {
+        marginTop: 15,
+        padding:10,
+        backgroundColor: '#e53232',
+    },
+    updateButton: {
+        marginTop: 15,
+        padding:10,
+        backgroundColor: '#07b72b',
+    },
+    buttonText: {
+        color: '#fff'
+    },
+    modalContainer: {
+        margin: 20,
+        backgroundColor: 'white',
+        borderRadius: 20,
+        padding: 35,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+    },
+    modalCenter: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    input: {
+        marginVertical: 15
+    },
+    addButton: {
+        marginTop: 10,
+        padding: 10,
+        backgroundColor: '#7c08e8',
+        marginBottom: 10,
+        borderRadius: 10
+    },
+    textWhite: {
+        color: '#fff'
+    },
 })
--- src/screens/AdminsScreen.js
+++ src/screens/AdminsScreen.js
@@ -1,35 +1,47 @@
 import React, {useState, useEffect} from 'react';
-import { Text, View, StyleSheet, FlatList, Alert } from 'react-native';
-import { Button } from 'react-native-paper';
+import {Text, Button, View, StyleSheet, FlatList, Alert, TouchableOpacity, Modal} from 'react-native';
+import {TextInput} from 'react-native-paper';
 import Constants from 'expo-constants';
 import * as Contantes from '../../utilis/constantes';
 import AdminCard from "../componenets/AdminCard";
 
 const AdminsScreen = ({}) => {
+    //Declaracion de variables a utilizar.
     const [adminData, setAdminData] = useState([]);
     const [idAdmin, setIdAdmin] = useState(0);
     const [nombreAdmin, setNombreAdmin] = useState('');
     const [apellidoAdmin, setApellidoAdmin] = useState('');
     const [correoAdmin, setCorreoAdmin] = useState('');
-    const [aliasAdmin, setAlias] = useState('');
-    const [claveAdmin, setClave] = useState('');
+    const [aliasAdmin, setAliasAdmin] = useState('');
+    const [claveAdmin, setClaveAdmin] = useState('');
+    const [repetirClaveAdmin, setRepetirClave] = useState('');
+
+    //Declaracion de variable para el modal
+    const [modalVisible, setModalVisible] = useState(false);
+
+    //Declaracion de variable para la ip
     const ip = Contantes.IP;
 
 
 // Efecto para cargar los detalles del carrito al cargar la pantalla
 useEffect(() => {
+    console.log('sdsd')
     fillCards();
 }, []);
 
 // Funcion para obtener los detalles del carrito desde servidor
 const fillCards = async ()=>{
+    // Mandamos la peticion al servidor
     try {
-        const response = await fetch(`${ip}/cofeeshop/api/services/admin/admin.php?action=readAll`, {
+        const response = await fetch(`http://${ip}/coffeeshop/api/services/admin/administrador.php?action=readAll`, {
             method: 'GET',
         });
 
+        // Obtenemos la respuesta de la api y la convertimos a json.
         const data = await response.json();
 
+        // Revisamos si la respuesta fue exitosa, de ser asi entonces le pasamos nuestra data a la variable setAadminData, pero si no solo mostrara
+        // un mensaje de error.
         if(data.status){
             setAdminData(data.dataset);
         }else{
@@ -39,14 +51,182 @@ const fillCards = async ()=>{
         console.error(error, 'Error desde catch');
         Alert.alert('Error productos', data.error)
     }
+}
 
+//Funcion para renderizar cala elemento del carrito
     const renderItem = ({ item }) => (
+        // Le pasamos nuestro componente de AdminCard que recibe como parametros
+        // el item y loadCard(Funcion para mandar la peticion a la api)
         <AdminCard
             item={item}
             loadedCards={fillCards}
+            updateList={setAdminData}
         />
     );
+
+    const createAdmin = async () => {
+        const formData = new FormData();
+        formData.append('nombreAdministrador', nombreAdmin);
+        formData.append('apellidoAdministrador', apellidoAdmin);
+        formData.append('correoAdministrador', correoAdmin);
+        formData.append('aliasAdministrador', aliasAdmin);
+        formData.append('claveAdministrador', claveAdmin);
+        formData.append('confirmarClave', repetirClaveAdmin);
+
+        const response = await fetch(`http://${ip}/coffeeshop/api/services/admin/administrador.php?action=createRow`, {
+            method: 'POST',
+            body: formData
+        });
+
+        const data = await response.json();
+        if (data.status){
+            Alert.alert('Éxito', data.message);
+            setModalVisible(false);
+
+        } else{
+            Alert.alert('Error', data.error);
+        }
     }
+
+
+// Mostramos las cards con la informacion de los admin en nuestra pantalla
+    return (
+        <View style={styles.container}>
+            <Text>Haz clic en el siguiente botón para poder agregar un administrador</Text>
+
+            <TouchableOpacity style={styles.addButton} onPress={()=> {setModalVisible(true)}}>
+                <Text style={styles.textWhite}>Agregar administrador</Text>
+            </TouchableOpacity>
+
+            {/*MODAL*/}
+            <Modal
+                animationType='slide'
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => {setModalVisible(!modalVisible)}}>
+
+                <View style={styles.modalCenter}>
+                    <View style={styles.modalContainer}>
+                        <Text style={styles.title}>Agregar Administrador</Text>
+                        <TextInput
+                            label='Nombre'
+                            value= {nombreAdmin}
+                            onChangeText={setNombreAdmin}
+                            style={styles.input}
+                        />
+                        <TextInput
+                            label='Apellido'
+                            value= {apellidoAdmin}
+                            onChangeText={setApellidoAdmin}
+                            style={styles.input}
+                        />
+                        <TextInput
+                            label='Correo'
+                            value= {correoAdmin}
+                            onChangeText={setCorreoAdmin}
+                            style={styles.input}
+                            keyboardType={"email-address"}
+                        />
+                        <TextInput
+                            label='Alias'
+                            value= {aliasAdmin}
+                            onChangeText={setAliasAdmin}
+                            style={styles.input}
+                        />
+                        <TextInput
+                            label='Clave'
+                            value= {claveAdmin}
+                            onChangeText={setClaveAdmin}
+                            style={styles.input}
+                            secureTextEntry={true}
+                        />
+                        <TextInput
+                            label='Repetir clave'
+                            value= {repetirClaveAdmin}
+                            onChangeText={setRepetirClave}
+                            style={styles.input}
+                            secureTextEntry = {true}
+                        />
+
+                        <View>
+                            <TouchableOpacity style={styles.addButton} onPress={()=>{createAdmin()}}>
+                                <Text style={styles.textWhite}>Guardar</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={styles.addButton} onPress={()=>{setModalVisible(false)}}>
+                                <Text style={styles.textWhite}>Cancelar</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </View>
+            </Modal>
+
+            {/*Revisamos si el adminData(Donde se guarda la informacion que viene de la api) viene con informacion*/}
+            {adminData.length > 0 ? (
+                <FlatList
+                    // Pasamos la informacion que se trajo de la api
+                    data={adminData}
+                    //Le pasamos nuestro componente personalizado de card.
+                    renderItem={renderItem}
+                    // Permitira identificar de forma unica cada elemento de la lista
+                    keyExtractor={(item) => item.id_administrador.toString()} />
+            ) : (
+                <Text>Ocurrió un error al obtener los datos</Text>
+            )}
+        </View>
+    );
+
 }
 
 export default AdminsScreen;
+
+//Estilos
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#ffffff',
+        paddingTop: Constants.statusBarHeight,
+        paddingHorizontal: 16,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginVertical: 16,
+        color: '#5C3D2E',
+    },
+    containerButtons: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    addButton: {
+        marginTop: 10,
+        padding: 10,
+        backgroundColor: '#7c08e8',
+        marginBottom: 10,
+        borderRadius: 10
+    },
+    textWhite: {
+        color: '#fff'
+    },
+    modalContainer: {
+        margin: 20,
+        backgroundColor: 'white',
+        borderRadius: 20,
+        padding: 35,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+    },
+    modalCenter: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    input: {
+        marginVertical: 15
+    }
+});
